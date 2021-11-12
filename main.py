0fa7e0e1505ff97e88430d6f2c2addde15d290ad
khadada <lkhadada@gmail.com>
2021-11-12 08:27:48 +0100

    I update some feature like checking user value

--- main.py
+++ main.py
@@ -13,19 +13,22 @@ for clr in colors:
     player.goto(-250, space_between - 200)
     space_between += 80
     turtles.append(player)
-
-print(turtles)
-is_end = False
-while not is_end:
+start = False
+if user_guess:
+    if user_guess in colors:
+        start = True
+    else:
+        print("restart the game because you didn't enter color value")
+while start:
     for turtle in turtles:
         if turtle.xcor() > 275:
-            is_end = True
+            start = False
             if turtle.pencolor() == user_guess:
-                print(f'You win: {turtle.pencolor()} is the winner')
+                print(f'You win: the {turtle.pencolor()} turtle is the winner')
             else:
-                print(f'You lose: {turtle.pencolor()} is the winner')
+                print(f'You lose: the {turtle.pencolor()} turtle is the winner')
 
-        turtle.forward(random.randint(1,7))
+        turtle.forward(random.randint(1,10))
 
 
 
